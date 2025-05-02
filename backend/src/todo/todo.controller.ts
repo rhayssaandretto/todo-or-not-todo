ce0ddccf7c6073cafa603ee8475a184a4b1daf40
Rhayssa Andretto <rha.andretto@gmail.com>
2025-05-02 04:05:58 -0300

    feat: implement service and controller

--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { TodoDto } from './dtos/todo.dto';
+import { UpdateTodoDto } from './dtos/update-todo';
 
-@Controller()
+@Controller('todos')
 export class TodoController {
   constructor(private readonly service: TodoService) {}
 
   @Get()
-  getHello(): string {
-    return this.service.getHello();
+  async findAll(): Promise<TodoDto[]> {
+    return await this.service.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<TodoDto> {
+    return await this.service.findById(id);
+  }
+
+  @Post()
+  async create(@Body() data: TodoDto): Promise<TodoDto> {
+    return await this.service.create(data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: UpdateTodoDto,
+  ): Promise<UpdateTodoDto> {
+    return await this.service.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<void> {
+    return await this.service.delete(id);
   }
 }
--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TodoRepository } from './todo.repository';
+import { TodoDto } from './dtos/todo.dto';
+import TodoAdapter from './todo.adapter';
+import { Todo } from './schema/todo.schema';
+import { UpdateTodoDto } from './dtos/update-todo';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    private readonly repository: TodoRepository,
+    private readonly adapter: TodoAdapter,
+  ) {}
+
+  async findAll(): Promise<Todo[]> {
+    return await this.repository.findAll();
+  }
+
+  async findById(id: string): Promise<Todo> {
+    if (!id) {
+      throw new NotFoundException('ID is required');
+    }
+
+    const todo = await this.repository.findById(id);
+
+    if (!todo) {
+      throw new NotFoundException(
+        `No task found with the provided ID: "${id}".`,
+      );
+    }
+
+    return todo;
+  }
+
+  async create(data: TodoDto): Promise<Todo> {
+    const todos: Todo = this.adapter.createToEntity(data);
+    return await this.repository.create(todos);
+  }
+
+  async update(id: string, data: UpdateTodoDto): Promise<UpdateTodoDto> {
+    await this.findById(id);
+
+    const todo: Todo = this.adapter.updateToEntity(data);
+
+    const updated = await this.repository.update(id, todo);
+
+    if (!updated) {
+      throw new NotFoundException(
+        `No task found with the provided ID: "${id}".`,
+      );
+    }
+
+    return this.adapter.entityToDto(updated);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+}
